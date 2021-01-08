b9d005ff8b73c03d8468c95483bae84a773daede
dedisutendi <dedi_sutendi20@yahoo.co.id>
2021-01-08 10:50:20 +0700

    initial commit

---
+++ public/js/resources_js_Pages_home_js.js
@@ -0,0 +1,30 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Home_js"],{
+
+/***/ "./resources/js/Pages/Home.js":
+/*!************************************!*\
+  !*** ./resources/js/Pages/Home.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+
+
+function Home() {
+  var foo = 'React';
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+    children: ["Hallo ", foo]
+  });
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
+
+/***/ })
+
+}]);
---
+++ resources/js/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    let foo = 'React'
+    return (
+        <div>
+            Hallo {foo}
+        </div>
+    )
+}
+
+export default Home
